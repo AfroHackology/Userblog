5f3b48df8daa4c545435546f43a0c5b7810bbc64
Gregory James <gregoryjms7@gmail.com>
2019-10-26 06:28:13 -0700

    complete basic blog front-end

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'GregJ',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 25, 2019'
+    },
+    {
+        'author': 'zulu',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'October 26, 2019'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
