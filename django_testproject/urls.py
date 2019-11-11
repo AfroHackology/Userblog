1f87743c809e7f8b8d24ddd6d78be6431264080b
Gregory James <gregoryjms7@gmail.com>
2019-11-11 08:29:33 -0800

    Add CRUD

--- django_testproject/urls.py
+++ django_testproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from users import views as user_views
 
 urlpatterns = [
@@ -26,3 +29,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
